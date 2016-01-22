56f862f2402c3385b905e1acefead30f7c8f2762
Komang Luce <KopiSusu@Komangs-MBP.home>
2016-01-21 23:13:39 -0500

    retrieving img and description from api, passed to dom

--- app/main/chef/chef.js
+++ app/main/chef/chef.js
@@ -12,9 +12,8 @@
     // ex. scope.domElements.meal = "whatever the meal name is"
     $scope.domElements = {};
 
-    // temp values until api works
+    // temp values still needed from API
     $scope.domElements.availablity = "Mon. 9/18";
-    $scope.domElements.mealDescription = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.";
     $scope.domElements.chefLocation = "Brooklyn, NY";
 
     //////////////////////////
@@ -27,7 +26,9 @@
           function(cooking) { 
             _cooking = cooking;
             $scope.domElements.meal = cooking.get("meal").get("name");
+            $scope.domElements.imgSrc = cooking.get("meal").get("imageURLS")[0];
             $scope.domElements.cook = cooking.get("cook").get("displayName");
+            $scope.domElements.mealDescription = cooking.get("meal").get("information");
           },
           function(errorPayload) {
             $log.error('failure loading movie', errorPayload);
