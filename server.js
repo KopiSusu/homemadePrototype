adf714bf5b667451f02330ba6b6c03424ff948df
Komang Luce <KopiSusu@Komangs-MBP.home>
2016-03-08 22:04:00 -0500

    secure server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    app = express();
 
 app.use(express.static(__dirname + '/'));
 
@@ -8,4 +8,18 @@ app.all('/*', function(req, res, next) {
     res.sendFile('index.html', { root: __dirname });
 });
 
-app.listen(443); //the port you want to use
+var server = require('https').Server(app);
+
+server.listen(443);
+
+// var express = require('express');
+// var app = express();
+
+// app.use(express.static(__dirname + '/'));
+
+// app.all('/*', function(req, res, next) {
+//     // Just send the index.html for other files to support HTML5Mode
+//     res.sendFile('index.html', { root: __dirname });
+// });
+
+// app.listen(443); //the port you want to use
